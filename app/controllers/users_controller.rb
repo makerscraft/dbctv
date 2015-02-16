e1b9b5c706d5e9a93005a18c7aaac00ee78004b4
BootCoder <bootcoder@gmail.com>
2015-02-15 22:56:08 -0800

    rm old gen files -- finish styles for basic admin flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
   # GET /users
   # GET /users.json
   def index
@@ -73,5 +73,10 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:username, :password_hash, :email)
+    end
+
+    def authenticate_user!
+      return true if session[:id]
+      redirect_to root_path
     end
 end
