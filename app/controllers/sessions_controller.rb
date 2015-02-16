6d82946bbc7f28cecad2361610e3b17afb0b2ecf
BootCoder <bootcoder@gmail.com>
2015-02-15 21:13:20 -0800

    clean up sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,20 +5,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    p "*{" * 80
+
     @user = User.find_by_email(params[:email])
-    p @user
-    p @user.password
-    p params[:password]
+
     if @user.password == params[:password_hash]
-      p "login success"
+      # p "login success"
       session[:id] = @user.id
       redirect_to slides_path
     else
-      p "login success"
+      # p "login failed"
       redirect_to login_path
     end
-    p session[:id]
+
   end
 
   def destroy
