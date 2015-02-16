39f9b86926ebf2c975903b3426ca61ae17578a12
BootCoder <bootcoder@gmail.com>
2015-02-15 21:11:16 -0800

    style login -- assign routing for session management -- add seed user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,17 @@ class SessionsController < ApplicationController
     p "*{" * 80
     @user = User.find_by_email(params[:email])
     p @user
+    p @user.password
     p params[:password]
     if @user.password == params[:password_hash]
+      p "login success"
       session[:id] = @user.id
+      redirect_to slides_path
+    else
+      p "login success"
+      redirect_to login_path
     end
     p session[:id]
-    redirect_to root_path
   end
 
   def destroy
